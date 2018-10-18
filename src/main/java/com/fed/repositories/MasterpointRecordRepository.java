7aca5ceab7e6b839e87fc4de6167d2b2bdd85c60
Esa Auranen <esa.auranen@iki.fi>
2018-10-18 22:27:15 +0300

    Add masterpoint record repository

---
+++ src/main/java/com/fed/repositories/MasterpointRecordRepository.java
@@ -0,0 +1,7 @@
+package com.fed.repositories;
+
+import com.fed.database.MasterpointRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MasterpointRecordRepository extends JpaRepository<MasterpointRecord, Integer> {
+}
---
+++ src/test/java/com/fed/database/MasterpointRecordTest.java
@@ -0,0 +1,17 @@
+package com.fed.database;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasterpointRecordTest {
+
+
+    @Before
+    public void removeAllMasterpoints(){
+
+    }
+
+}
