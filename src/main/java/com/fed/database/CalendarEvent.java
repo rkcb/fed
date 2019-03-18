06389344b5b24c725e6fcd591e958246bf88ee81
Esa Auranen <esa.auranen@iki.fi>
2019-03-18 15:48:35 +0200

    Update the calendar Todo list

--- src/main/java/com/fed/database/CalendarEvent.java
+++ src/main/java/com/fed/database/CalendarEvent.java
@@ -32,7 +32,6 @@ public class CalendarEvent {
 
     public static CalendarEvent create(String title, Timestamp start){
 
-
         CalendarEvent calendarEvent = new CalendarEvent();
 
         calendarEvent.title = title;
