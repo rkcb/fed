61026b417b5949a6867f51136198f963e34b3c06
Esa Auranen <esa.auranen@iki.fi>
2019-03-18 15:15:32 +0200

    Modify calendar event entity to use ms as start

---
+++ src/main/java/com/fed/controllers/FileUploadController.java
@@ -0,0 +1,58 @@
+package com.fed.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+public class FileUploadController {
+
+//    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(/*StorageService storageService*/) {
+//        this.storageService = storageService;
+    }
+
+    @GetMapping("/upload")
+    public String listUploadedFiles(Model model) throws IOException {
+
+      /*  model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
+*/
+        return "uploadForm";
+    }
+/*
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+            RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+*/
+
+}
