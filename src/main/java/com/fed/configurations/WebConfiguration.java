d6646f9c5196b5cd7e7c9a8d227a4327488213f8
Esa Auranen <esa.auranen@iki.fi>
2019-06-08 13:27:11 +0300

    Add Liquibase to pom.xml

--- src/main/java/com/fed/configurations/WebConfiguration.java
+++ src/main/java/com/fed/configurations/WebConfiguration.java
@@ -3,14 +3,18 @@ package com.fed.configurations;
 import com.fed.converters.IsoStringToLocalDateTime;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry){
         registry.addConverter(new IsoStringToLocalDateTime());
     }
+
+
 
 }
