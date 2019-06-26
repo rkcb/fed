a27f692972ea1e70a7d7b88c3ece61908bfc4df7
Esa Auranen <esa.auranen@iki.fi>
2019-06-26 18:46:38 +0300

    Add a Data source test

--- src/test/java/com/fed/FedApplicationTests.java
+++ src/test/java/com/fed/FedApplicationTests.java
@@ -1,25 +1,24 @@
 package com.fed;
 
-import com.fed.database.CalendarEvent;
-import com.fed.repositories.CalendarEventRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.validation.constraints.AssertTrue;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@DataJpaTest
 public class FedApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void fedTest(){
+        Assert.assertNotNull(dataSource);
+    }
+
 }
--- src/test/java/com/fed/repositories/CalendarEventRepositoryTests.java
+++ src/test/java/com/fed/repositories/CalendarEventRepositoryTests.java
@@ -4,23 +4,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@DataJpaTest
+//@DataJpaTest
 @AutoConfigureMockMvc
 public class CalendarEventRepositoryTests {
 
@@ -37,7 +28,6 @@ public class CalendarEventRepositoryTests {
 
     @Test
     public void shouldReturnRepositoryIndex() throws Exception {
-
     }
 //    @Test
 //    public void shouldReturnRepositoryIndex() throws Exception {
