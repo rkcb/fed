63dc1016158505c1ee4e74d98846d471fef8cba2
Esa Auranen <esa.auranen@iki.fi>
2019-06-29 11:19:09 +0300

    Add a web configuration test class

--- src/main/java/com/fed/configurations/WebConfiguration.java
+++ src/main/java/com/fed/configurations/WebConfiguration.java
@@ -1,18 +1,60 @@
 package com.fed.configurations;
 
-import com.fed.converters.IsoStringToLocalDateTime;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-@EnableWebMvc
-public class WebConfiguration implements WebMvcConfigurer {
+import javax.sql.DataSource;
 
-    @Override
-    public void addFormatters(FormatterRegistry registry){
-        registry.addConverter(new IsoStringToLocalDateTime());
+@Configuration
+@EnableWebSecurity
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    private JdbcUserDetailsManager jdbcUserDetailsManager;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    public JdbcUserDetailsManager getJdbcUserDetailsManager(){
+        return jdbcUserDetailsManager;
+    }
+
+//    @Bean
+//    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+//        return jdbcUserDetailsManager;
+//    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        jdbcUserDetailsManager =
+                auth
+                        .jdbcAuthentication()
+                        .dataSource(dataSource)
+                        .passwordEncoder(passwordEncoder())
+                        .getUserDetailsService();
+    }
 
 
 }
---
+++ src/test/java/com/fed/configurations/WebConfigurationTest.java
@@ -0,0 +1,29 @@
+package com.fed.configurations;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebConfigurationTest {
+
+
+//    @TestConfiguration
+//    static class WebConf {
+//
+//
+//
+//
+//    }
+
+
+    @Test
+    public void jdbcUserDetailsManagerTest(){
+
+        Assert.assertTrue(true);
+
+    }
+}
