b923c0432cf2e78a1af69ed7001d5055436db4f1
Esa Auranen <esa.auranen@iki.fi>
2019-08-15 15:11:41 +0300

    Add validations to users post data

--- src/main/java/com/fed/data/UsersData.java
+++ src/main/java/com/fed/data/UsersData.java
@@ -1,21 +1,48 @@
 package com.fed.data;
 
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @Data
 public class UsersData {
 
-    @Email(message = "invalid email address")
+    @Autowired
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
+    private JdbcUserDetailsManager manager;
+
+    @Email(message = "Invalid email address")
     String username;
-    @Min(value = 2, message = "password must be at least 2 chars")
+
+    @Size(min = 2, message = "Password must be at least 2 chars")
     String password;
+
+    @Size(min = 2, message = "Password must be at least 2 chars")
     String password2;
+
     String oldPassword;
+
     boolean enabled;
+
+    @AssertTrue(message = "Passwords are not equal")
+    public boolean validPasswords(){
+        return password != null && password2 != null &&
+                password.equals(password2);
+    }
+
+    @AssertTrue(message = "Username is not available")
+    public boolean validUsername(){
+        return !manager.userExists(username);
+    }
 }
 
 
