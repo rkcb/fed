5141ef69c6d7fd6af7c4f79d2700235ff1d132ac
Esa Auranen <esa.auranen@iki.fi>
2019-08-28 20:15:16 +0300

    Update Users data object

--- src/main/java/com/fed/data/UsersData.java
+++ src/main/java/com/fed/data/UsersData.java
@@ -1,24 +1,18 @@
 package com.fed.data;
 
 
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
 @Data
 public class UsersData {
 
-    @Autowired
-    @Setter(AccessLevel.NONE)
-    @Getter(AccessLevel.NONE)
-    private JdbcUserDetailsManager manager;
+//    @Autowired
+//    @Setter(AccessLevel.NONE)
+//    @Getter(AccessLevel.NONE)
+//    private JdbcUserDetailsManager manager;
 
     @Email(message = "Invalid email address")
     String username;
@@ -33,16 +27,16 @@ public class UsersData {
 
     boolean enabled;
 
-    @AssertTrue(message = "Passwords are not equal")
-    public boolean validPasswords(){
-        return password != null && password2 != null &&
-                password.equals(password2);
-    }
-
-    @AssertTrue(message = "Username is not available")
-    public boolean validUsername(){
-        return !manager.userExists(username);
-    }
+//    @AssertTrue(message = "Passwords are not equal")
+//    public boolean validPasswords(){
+//        return password != null && password2 != null &&
+//                password.equals(password2);
+//    }
+//
+//    @AssertTrue(message = "Username is not available")
+//    public boolean validUsername(){
+//        return !manager.userExists(username);
+//    }
 }
 
 
